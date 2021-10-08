e41f3a28cfe12fdc651988ab01d53cf6fb48e73e
leblanmonster <73737394+leblanmonster@users.noreply.github.com>
2021-10-08 22:21:22 +0200

    update ti 1.1.1

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ module.exports = class db {
             if (opts.init) {
                 fs.writeFileSync(this.path2, "{}", function (err) {
                     if (err) return console.log(err);
-                    console.log("is good")
                 });
                 this.db = {}
             }
@@ -36,7 +35,6 @@ module.exports = class db {
         let str = JSON.stringify(this.db)
         fs.writeFileSync(this.path2, str, function (err) {
             if (err) return console.log(err);
-            console.log(str)
         });
         this.getall(1)
     }
@@ -55,29 +53,70 @@ module.exports = class db {
     set(path, data) {
         if (!data) throw "data must be specified"
         let str = this.init(path)
-        if(Array.isArray(data)){
+        if (Array.isArray(data)) {
             let str = this.init(path)
-            console.log(data)
-            eval("this.db" + str + " = ["+data+"]")
+            eval("this.db" + str + " = [" + data + "]")
         }
         else if (typeof data == "object") {
-            let str = this.init(path)
-            console.log("hey")
-            str += "[" + Object.keys(data)[0] + "]"
-            console.log(str)
-            if (typeof data[Object.keys(data)[0]] == "object") {
-                cconsole.log("salut")
-                let data2 = Object.values(data)[0]
-                data2.join(",")
-                data2.slice(0, -1)
-                eval("this.db" + str + " = [" + data2 + "]")
-            }
-
-            else if (typeof Object.values(data)[0] == "string") {
-                eval("this.db" + str + " =  " + '`' + Object.values(data)[0] + '`')
-            } else {
-                eval("this.db" + str + " = " + Object.values(data)[0])
-            }
+            Object.keys(data).forEach((el, i) => {
+                let str = this.init(path)
+                str += "[" + el + "]"
+                if (typeof data[el] == "object") {
+                    let data2 = Object.values(data)[i]
+                    let str = this.init(path)
+                    str += "[`" + el + "`]"
+                    eval("this.db" + str + " = "+da)
+                    Object.keys(data2).forEach((el2, f) => {
+                        let str = this.init(path)
+                        str+="[`"+el+"`]"
+                        if (Array.isArray(Object.values(data2)[f])) {
+                            let str2 = Object.values(data2)[f].join(",")
+                            str += "[`" + el2 + "`]"
+                            if(Object.values(data2)[f].length == 0) {
+                                eval("this.db" + str + " = []")
+                                return
+                            }
+                            eval("this.db" + str + " = [" + str2 + "]")
+                        }
+                        else if (typeof Object.values(data2)[f] == "string") {
+                            str += "[`" + el2 + "`]"
+                            eval("this.db" + str + " =  " + '`' + Object.values(data2)[f] + '`')
+                        }
+                        else if (typeof Object.values(data2)[f] == "boolean") {
+                            str += "[`" + el2 + "`]"
+                            eval("this.db" + str + " =  " + Object.values(data2)[f])
+                        }
+                        else {
+                            str += "[`" + el2 + "`]"
+                            eval("this.db" + str + " = " + Object.values(data2)[f])
+                        }
+                    })
+                }
+                else if (Array.isArray(Object.values(data)[i])) {
+                    let str = this.init(path)
+                    let str2 = Object.values(data)[i].join(",")
+                    if(Object.values(data)[i].length == 0) {
+                        eval("this.db" + str + " = []")
+                        return
+                    }
+                    eval("this.db" + str + " = [" + str2 + "]")
+                }
+                else if (typeof Object.values(data)[i] == "string") {
+                    let str = this.init(path)
+                    str += "[`" + el + "`]"
+                    eval("this.db" + str + " =  " + '`' + Object.values(data)[i] + '`')
+                }
+                else if (typeof Object.values(data)[i] == "boolean") {
+                    let str = this.init(path)
+                    str += "[`" + el + "`]"
+                    eval("this.db" + str + " =  " + Object.values(data)[i])
+                }
+                else {
+                    let str = this.init(path)
+                    str += "[`" + el + "`]"
+                    eval("this.db" + str + " = " + Object.values(data)[i])
+                }
+            })
         }
         else if (typeof data == "string") {
             eval("this.db" + str + " = `" + data + "`")
@@ -110,19 +149,19 @@ module.exports = class db {
             str += "[`" + paths[i] + "`]"
             eval("if(!this.db" + str + "){this.db" + str + " = {}}")
         }
-        eval("if(!this.db"+str+")throw `the path doesnt exist`")
+        eval("if(!this.db" + str + ")throw `the path doesnt exist`")
         eval("this.db" + str + ".push(" + dataa + ")")
         this.write()
     }
     delete(path) {
         let str = this.init(path)
-        eval("if(!this.db"+str+")throw `the path doesnt exist`")
+        eval("if(!this.db" + str + ")throw `the path doesnt exist`")
         eval("delete this.db" + str)
         this.write()
         return true
     }
     add(path, number) {
-        if (typeof number != "number") return
+        if (typeof number != "number") return false
         let str = this.init(path)
         let paths = path.split("/")
         let key = path[paths.length - 1]
@@ -133,6 +172,7 @@ module.exports = class db {
         paths = paths.join("/")
         eval("if(typeof this.db" + str + "== 'object' ){this.set(`" + paths + "`, { " + key + " : " + number + "} )}else{ this.db" + str + "+= " + number + "}")
         this.write()
+        return true
     }
     has(path) {
         this.getall(1)
